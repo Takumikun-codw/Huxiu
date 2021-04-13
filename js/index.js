4a96487a775c735fe1f955cab7e3ee720a253f7f
Takumikun-codw <2390896268@qq.com>
2021-04-13 15:59:03 +0800

    0.5

--- js/index.js
+++ js/index.js
@@ -3,13 +3,20 @@ window.addEventListener('load', function() {
   var arrow_r = document.querySelector('.arrow-r');
   var focus = document.querySelector('.bigbanner');
   var focusWidth = focus.offsetWidth;
+  var n = 0;
+  var circle = 0;
   focus.addEventListener('mouseenter', function() {
     arrow_l.style.display = 'block';
     arrow_r.style.display = 'block';
+    clearInterval(timer);
+    timer = null;
   })
   focus.addEventListener('mouseleave', function() {
     arrow_l.style.display = 'none';
     arrow_r.style.display = 'none';
+    timer = setInterval(function(){
+      arrow_r.click();
+    }, 3000)
   })
   var ul = focus.querySelector('ul');
   var ol = focus.querySelector('.circle');
@@ -24,13 +31,58 @@ window.addEventListener('load', function() {
       }
       this.className = 'current';
       var index = this.getAttribute('index');
+      n = index;
+      circle = index;
       animate(ul, -index * focusWidth);
     })
   }
   ol.children[0].className = 'current';
-  var n = 0;
+  var first = ul.children[0].cloneNode(true);
+  ul.appendChild(first);
+  var flag = true;
   arrow_r.addEventListener('click', function() {
-    n++;
-    animate(ul, -n * focusWidth);
+    if(flag) {
+      flag = false;
+      if(n == ul.children.length - 1) {
+        ul.style.left = 0;
+        n = 0;
+      }
+      n++;
+      animate(ul, -n * focusWidth, function() {
+        flag = true;
+      });
+      circle++;
+      if(circle == ol.children.length) {
+        circle = 0;
+      }
+      circleChange();
+    }    
   })
+  arrow_l.addEventListener('click', function() {
+    if(flag) {
+      flag = false;
+      if(n == 0) {
+        ul.style.left = -(ul.children.length - 1) * focusWidth + 'px';
+        n = ul.children.length - 1;
+      }
+      n--;
+      animate(ul, -n * focusWidth, function() {
+        flag = true;
+      });
+      circle--;
+      if(circle < 0) {
+        circle = ol.children.length - 1;
+      }
+      circleChange();
+    }
+  })
+  function circleChange() {
+    for(var i = 0; i < ol.children.length; i++) {
+      ol.children[i].className = '';
+    }
+    ol.children[circle].className = 'current';
+  }
+  var timer = setInterval(function(){
+    arrow_r.click();
+  }, 10000)
 })
